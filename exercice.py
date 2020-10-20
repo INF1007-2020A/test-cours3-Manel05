9d01a10e27adf27a020fdaaaa6c090f02f0f2354
Manel05 <Manel05@users.noreply.github.com>
2020-10-20 10:56:31 -0400

    done

--- exercice.py
+++ exercice.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import string
 def capitaliser_pays(nom):
     # TODO completer la fonction
-    return nom
+    return nom.capitalize()
 
 
 if __name__ == '__main__':
@@ -21,5 +22,5 @@ if __name__ == '__main__':
     ]
     for i in range(len(pays)):
         pays[i] = capitaliser_pays(pays[i])
-
+        #print(pays[i])
     print(pays)
